c2709a46923da567eebcc6a52f091b8d1659d09d
RayhanAhmed0155 <54303148+RayhanAhmed0155@users.noreply.github.com>
2019-11-06 18:37:37 +0600

    Add files via upload

---
+++ Registration/WT.php
@@ -0,0 +1,16 @@
+<?php
+$username=$_POST['uname'];
+$password=$_POST['password'];
+$dob=$_POST['date'];
+$add=$_POST['tarea'];
+$dept=$_POST['dept'];
+$gender=$_POST['gender'];
+$option=$_POST['option'];
+
+print_r($_POST);
+//echo $username." ".$password." ".$dob." ".$add." ".$dept." ".$gender." " ";
+
+
+
+
+?>
